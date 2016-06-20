545509fcccc9431d27b033e74f6015b477a734fc
Sambit Samal <itsme.sambit1991@gmail.com>
2016-06-20 16:55:52 +0530

    Added Salt String Features to enhance security

--- fullstophash.py
+++ fullstophash.py
@@ -10,20 +10,104 @@ It is just for a sample
 USES
 * Creates an UNBREAKABLE AND UNCRACKABLE, STILL UNIQUE hash string of variable digest size and length based on the outermost hash algorithm used (In our case the message length, the digest size and block size is equal to that of sha512). Thus it becomes unpredictable for the crackers if SENSITIVE PASSWORDS are stored using MY FullStopHash
 
+PUBLIC FUNCTION USED :
+1) hexdigest - Creates an hexadecimal representation of the fullstophash (WITHOUT SALT STRING)
+2) digest - Creates an digested version of the fullstophash (WITHOUT SALT STRING)
+3) b64hexdigest - Base64 encoded Hexadecimal representation of the fullstophash (WITHOUT SALT STRING)
+4) b64digest - Base64 encoded digested version of the fullstophash (WITHOUT SALT STRING)
+5) hexdigest_encrypt - Creates an hexadecimal representation of the fullstophash (WITH SALT STRING)
+6) digest_encrypt - Creates an digested version of the fullstophash (WITH SALT STRING)
+7) b64hexdigest_encrypt - Base64 encoded Hexadecimal representation of the fullstophash (WITH SALT STRING)
+8) b64digest_encrypt - Base64 encoded digested version of the fullstophash (WITH SALT STRING)
+
 '''
 
 import hashlib
 import base64
 class fullstophash:
 	@staticmethod
+	def mainhexdigest(SrcString):
+		DestHash = str(SrcString)
+		DestHash = hashlib.md5(DestHash).hexdigest()
+		DestHash = hashlib.sha1(DestHash).hexdigest()
+		DestHash = hashlib.sha224(DestHash).hexdigest()
+		DestHash = hashlib.sha384(DestHash).hexdigest()
+		DestHash = hashlib.sha256(DestHash).hexdigest()
+		DestHash = hashlib.sha512(DestHash).hexdigest()
+		return str(DestHash)
+	@staticmethod
+	def maindigest(SrcString):
+		DestHash = str(SrcString)
+		DestHash = hashlib.md5(DestHash + salt).digest()
+		DestHash = hashlib.sha1(DestHash + salt).digest()
+		DestHash = hashlib.sha224(DestHash + salt).digest()
+		DestHash = hashlib.sha384(DestHash + salt).digest()
+		DestHash = hashlib.sha256(DestHash + salt).digest()
+		DestHash = hashlib.sha512(DestHash + salt).digest()
+		return str(DestHash)
+		
+	@staticmethod
+	def hexdigest_encrypt(SrcString):
+		salt = fullstophash.mainhexdigest(SrcString)
+		DestHash = fullstophash.mainhexdigest(SrcString + salt)
+		return str(DestHash)
+	
+	@staticmethod
+	def digest_encrypt(SrcString):
+		salt = fullstophash.maindigest(SrcString)
+		DestHash = fullstophash.maindigest(SrcString + salt)
+		return str(DestHash)
+		
+	@staticmethod
+	def b64digest_encrypt(SrcString):
+		salt = fullstophash.maindigest(SrcString)
+		salt = base64.b64encode(salt)
+		DestHash = fullstophash.maindigest(SrcString + salt)
+		DestHash = base64.b64encode(DestHash)
+		return str(DestHash)
+		
+	@staticmethod
+	def b64hexdigest_encrypt(SrcString):
+		salt = fullstophash.mainhexdigest(SrcString)
+		salt = base64.b64encode(salt)
+		DestHash = fullstophash.mainhexdigest(SrcString + salt)
+		DestHash = base64.b64encode(DestHash)
+		return str(DestHash)
+
+	@staticmethod
 	def hexdigest(SrcString):
-		return hashlib.sha512(hashlib.sha256(hashlib.sha384(hashlib.sha224(hashlib.sha1(hashlib.md5(SrcString).hexdigest()).hexdigest()).hexdigest()).hexdigest()).hexdigest()).hexdigest()
+		DestHash = fullstophash.mainhexdigest(SrcString)
+		return str(DestHash)
+
 	@staticmethod
 	def digest(SrcString):
-		return hashlib.sha512(hashlib.sha256(hashlib.sha384(hashlib.sha224(hashlib.sha1(hashlib.md5(SrcString).digest()).digest()).digest()).digest()).digest()).digest()
+		DestHash = fullstophash.maindigest(SrcString)
+		return str(DestHash)
+
 	@staticmethod
 	def b64digest(SrcString):
-		return base64.b64encode(hashlib.sha512(hashlib.sha256(hashlib.sha384(hashlib.sha224(hashlib.sha1(hashlib.md5(SrcString).digest()).digest()).digest()).digest()).digest()).digest())
+		DestHash = fullstophash.maindigest(SrcString)
+		DestHash = base64.b64encode(DestHash)
+		return str(DestHash)
+
 	@staticmethod
 	def b64hexdigest(SrcString):
-		return base64.b64encode(hashlib.sha512(hashlib.sha256(hashlib.sha384(hashlib.sha224(hashlib.sha1(hashlib.md5(SrcString).hexdigest()).hexdigest()).hexdigest()).hexdigest()).hexdigest()).hexdigest())
+		DestHash = fullstophash.mainhexdigest(SrcString)
+		DestHash = base64.b64encode(DestHash)
+		return str(DestHash)
+
+'''	def listexistinlist(a,b):
+		for i in a:
+			if i in b:
+				return True
+		return False
+	def hexdigest_file(filepath):
+		with open(filePath, 'rb') as fh:
+			m = hashlib.md5()
+			while True:
+				data = fh.read(8192)
+				if not data:
+					break
+				m.update(data)
+        return m.hexdigest()
+'''
