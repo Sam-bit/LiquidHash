9b67a046bf187709a29eb72bae9f1944a05b523a
Sambit Samal <itsme.sambit1991@gmail.com>
2016-07-04 13:44:19 +0530

    Updated due to latest changes made

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from fullstophash import *
-#print fullstophash.digest("Sambit")+"\n"
+print fullstophash.digest("Sambit")+"\n"
 print fullstophash.hexdigest('Sambit')+'\n'	
-print fullstophash.hexdigest_encrypt('Sambit')+'\n'	
-#print len(fullstophash.hexdigest("Sambit"))
-#print fullstophash.b64digest("Sambit")+"\n"
-#print fullstophash.b64hexdigest("Sambit")+"\n"
-#print fullstophash('Sambit').fullstopdigest([sha1])+"\n"
-#print hashlib.md5("").hexdigest()
+print fullstophash.digest_crypted('Sambit')+'\n'
+print fullstophash.hexdigest_crypted('Sambit')+'\n'
+print fullstophash.hexdigest_encoded("Sambit")+"\n"
+print fullstophash.digest_encoded("Sambit")+"\n"
+print fullstophash.digest_encrypted('Sambit')+'\n'
+print fullstophash.hexdigest_encrypted('Sambit')+'\n'
